1b2db782e5fdf764c0013a497eb236d581922f51
Anthony Petrov <beavean@gmail.com>
2022-11-01 18:13:13 +0200

    add AddLocationController with location search methods

---
+++ Uber/Controller/Settings/AddLocationController.swift
@@ -0,0 +1,101 @@
+//
+//  AddLocationController.swift
+//  Uber
+//
+//  Created by Beavean on 01.11.2022.
+//
+
+import UIKit
+import MapKit
+
+private let reuseIdentifier = "Cell"
+
+class AddLocationController: UITableViewController {
+    
+    //MARK: - Properties
+    
+    private let searchBar = UISearchBar()
+    private let searchCompleter = MKLocalSearchCompleter()
+    private var searchResults = [MKLocalSearchCompletion]() {
+        didSet {
+            tableView.reloadData()
+        }
+    }
+    private let type: LocationType
+    private let location: CLLocation
+    
+    //MARK: - Lifecycle
+    
+    init(type: LocationType, location: CLLocation) {
+        self.type = type
+        self.location = location
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureTableView()
+        configureSearchBar()
+        configureSearchCompleter()
+    }
+    
+    //MARK: - Helpers
+    
+    private func configureTableView() {
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: reuseIdentifier)
+        tableView.tableFooterView = UIView()
+        tableView.rowHeight = 60
+        tableView.addShadow()
+    }
+    
+    private func configureSearchBar() {
+        searchBar.sizeToFit()
+        searchBar.delegate = self
+        navigationItem.titleView = searchBar
+    }
+    
+    private func configureSearchCompleter() {
+        let region = MKCoordinateRegion(center: location.coordinate, latitudinalMeters: 2000, longitudinalMeters: 2000)
+        searchCompleter.region = region
+        searchCompleter.delegate = self
+    }
+}
+
+//MARK: - UITableViewDelegate/DataSource
+
+extension AddLocationController {
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return searchResults.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: reuseIdentifier)
+        let result = searchResults[indexPath.row]
+        cell.textLabel?.text = result.title
+        cell.detailTextLabel?.text = result.subtitle
+        return cell
+    }
+}
+
+//MARK: - UISearchBarDelegate
+
+extension AddLocationController: UISearchBarDelegate {
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        searchCompleter.queryFragment = searchText
+    }
+}
+
+//MARK: - MKLocalSearchCompleterDelegate
+
+extension AddLocationController: MKLocalSearchCompleterDelegate {
+    
+    func completerDidUpdateResults(_ completer: MKLocalSearchCompleter) {
+        searchResults = completer.results
+    }
+}
--- Uber/Controller/Settings/SettingsController.swift
+++ Uber/Controller/Settings/SettingsController.swift
@@ -37,6 +37,7 @@ class SettingsController: UITableViewController {
     //MARK: - Properties
     
     private let user: User
+    private let locationManager = LocationHandler.shared.locationManager
     
     private lazy var infoHeader: UserInfoHeader = {
         let frame = CGRect(x: 0, y: 0, width: view.frame.width, height: 100)
@@ -74,6 +75,7 @@ class SettingsController: UITableViewController {
         tableView.register(LocationCell.self, forCellReuseIdentifier: reuseIdentifier)
         tableView.backgroundColor = .white
         tableView.tableHeaderView = infoHeader
+        tableView.tableFooterView = UIView()
     }
     
     private func configureNavigationBar() {
@@ -120,6 +122,11 @@ extension SettingsController {
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        guard let type = LocationType(rawValue: indexPath.row) else { return }
+        guard let type = LocationType(rawValue: indexPath.row),
+              let location = locationManager?.location
+        else { return }
+        let controller = AddLocationController(type: type, location: location)
+        let navigation = UINavigationController(rootViewController: controller)
+        self.present(navigation, animated: true)
     }
 }
